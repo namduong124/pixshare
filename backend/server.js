e8b704433a20a3fbd16eeb9531d2f807b7d33ad7
namduong124 <namduongn306@gmail.com>
2026-04-03 00:42:13 +0700

    sua

--- backend/server.js
+++ backend/server.js
@@ -30,14 +30,14 @@ app.get('/', (req, res) => {
 });
 
 // --- DATABASE CONNECTION ---
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI)
     .then(() => {
         console.log('✅ Connected to MongoDB');
         // Chỉ chạy server khi đã kết nối Database thành công
-        app.listen(PORT, () => {
+        app.listen(PORT, '0.0.0.0', () => {
             console.log(`🚀 Server is running on: http://localhost:${PORT}`);
         });
     })
